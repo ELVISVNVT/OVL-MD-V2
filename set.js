7a1bb3f4da43728e2229306184d1417e01a42ed3
ELVISVNVT <168463999+ELVISVNVT@users.noreply.github.com>
2025-06-24 10:58:03 +0000

    Update set.js

--- set.js
+++ set.js
@@ -2,10 +2,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  PREFIXE: process.env.PREFIXE || "",
-  NOM_OWNER: process.env.NOM_OWNER || "Ainz",
-  NUMERO_OWNER: process.env.NUMERO_OWNER || "",
-  MODE: process.env.MODE || "public",
+  PREFIXE: process.env.PREFIXE || ".",
+  NOM_OWNER: process.env.NOM_OWNER || "VNVT",
+  NUMERO_OWNER: process.env.NUMERO_OWNER || "22891983438",
+  MODE: process.env.MODE || "private",
   SESSION_ID: process.env.SESSION_ID || "ovl",
   LEVEL_UP: process.env.LEVEL_UP || "non",
   STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "Wa-sticker",
